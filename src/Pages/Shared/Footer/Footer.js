709cbc996818966486c647c4df1aefebfdd86ace
mitanvir05 <tanvir.cse05@gmail.com>
2022-04-19 03:15:45 +0600

    footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer
       style={{ marginTop: "auto" }}
-      className="bg-dark text-white text-center mt-5"
+      className="bg-dark text-white text-center "
     >
       <p>
         <small> &copy; {year} </small>
